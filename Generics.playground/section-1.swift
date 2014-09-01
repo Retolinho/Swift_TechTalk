34b66b3c965f3341f0942204e1477482e624cbdc
Patrick Jayet <pajai@extrabright.com>
2014-09-01 11:18:17 +0200

    extended playground for Generics

--- Generics.playground/section-1.swift
+++ Generics.playground/section-1.swift
@@ -3,7 +3,47 @@
 import UIKit
 
 
-// TODO
-// but keep it short, it's basically like in Java
+// Basically similar to Java
 
+
+// Generics Parameter für Methoden
+
+func print<T: Printable>(t: T) {
+    print(t.description)
+}
+
+print(5.0)
+
+
+// Und für Klassen
+
+class Stack<T> {
+    private var values: [T] = []
+
+    func push(t: T) {
+        values.append(t)
+    }
+    
+    func pop() -> T? {
+        let n = countElements(values)
+        if n > 0 {
+            let t: T = values[n-1]
+            values.removeAtIndex(n-1)
+            return t
+        }
+        return nil;
+    }
+    
+    func isEmpty() -> Bool {
+        return countElements(values) == 0
+    }
+}
+
+
+let s = Stack<Int>()
+s.push(5)
+s.push(10)
+while (!s.isEmpty()) {
+    println(s.pop())
+}
 
